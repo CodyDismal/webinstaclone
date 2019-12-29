273fd05de1aecc8bcade5c11305944f522270803
Oleksandr Fedoruk <oleksandr.fedoruk@britenet.com.pl>
2019-12-29 02:54:17 +0100

    Poprawione migarcje oraz relacje poiędzy tabelami; Formularz rejestracji;

--- frontend/src/components/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm.vue
@@ -4,32 +4,71 @@
             <form>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                           placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
+                    <input type="email" class="form-control" id="exampleInputEmail1"
+                           placeholder="Enter email" v-model="email">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Hasło</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hasło">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hasło"
+                           v-model="password">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Powtórz hasło</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Powtórz hasło">
+                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Powtórz hasło"
+                           v-model="passwordCopy">
                 </div>
                 <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="agreed">
+                    <label class="form-check-label" for="exampleCheck1">Zgadzam się z RODO, Cookies i
+                        t.d.</label>
                 </div>
-                <button type="button" class="btn btn-primary">Załóż konto</button>
+                <button type="button" class="btn btn-primary" :disabled='!passwordIsOk' @click="register">
+                    Załóż konto
+                </button>
             </form>
+        </div>
+        <div class="alert alert-danger" v-if='password !== passwordCopy'>
+            Hasła muszą się zgadzać!
+        </div>
+        <div class="alert alert-danger" v-if='password.length <= 3'>
+            Hasło musi mieć przynajmniej 4 symboli!
+        </div>
+        <div class="alert alert-success" v-if='registrationSuccess'>
+            Rejestracja powiodłą się! Sprawdź mail by aktywować konto!
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "RegistrationForm"
+        name: "RegistrationForm",
+        data() {
+            return {
+                email: '',
+                password: '',
+                passwordCopy: '',
+                agreed: false,
+                registrationSuccess: false
+            }
+        },
+        computed: {
+            passwordIsOk() {
+                return this.password.length > 3 && this.password === this.passwordCopy && this.agreed === true;
+            }
+        },
+        methods: {
+            register() {
+                const self = this;
+                this.axios.post('/auth/register', {
+                    email: self.email,
+                    password: self.password
+                }).then(result => {
+                    self.registrationSuccess = true;
+                }).catch(error => {
+                    console.log(error);
+                })
+            }
+        }
     }
 </script>
 
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,58 +1,22 @@
 const cors = require('cors');
 
 const express = require('express');
-const models = require('../models');
-const sha256 = require('../helpers/sha256');
+const logic = require('../logic/auth');
+
 const router = express.Router();
 
 router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    if (email && password) {
-        models.User.findOne({
-            where: {
-                email: email,
-                password: sha256(password)
-            }
-        }).then(user => {
-            if (user) {
-                req.session.loggedin = true;
-                req.session.email = email;
-                return res.send();
-            }
-            res.status(500).send('Błędne hasło lub e-mail!');
-        })
-    } else {
-        res.status(500).send('Błędne hasło lub e-mail!');
-        res.end();
-    }
+    return logic.loginUser(email, password, req, res);
 });
 
 
 router.post('/register', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const nickName = req.body.nickName;
-
-    if (email && password) {
-        models.User.findOne({
-            where: {
-                email: email,
-                password: sha256(password)
-            }
-        }).then(user => {
-            if (user) {
-                req.session.loggedin = true;
-                req.session.email = email;
-                return res.send();
-            }
-            res.status(500).send('Błędne hasło lub e-mail!');
-        })
-    } else {
-        res.status(500).send('Błędne hasło lub e-mail!');
-        res.end();
-    }
+    return logic.registerUser(email, password, req, res);
 });
+
+
 module.exports = router;
